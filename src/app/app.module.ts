1d7a7c1aa7ba63960f169a91c0143041a255f385
Anirudh <anirudhkalva09@gmail.com>
2020-05-16 12:52:05 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+     HeaderComponent,
+    FooterComponent,
+     HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    DishdetailComponent
+    ],
+  imports: [
+    BrowserModule,
+     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+      AppRoutingModule,
+     MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  providers: [DishService,
+PromotionService
+],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
